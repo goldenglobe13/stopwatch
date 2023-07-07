0be8cd5c996032ddd6e006957500061911614874
golden <amdexec@gmail.com>
2023-07-07 15:21:12 +0330

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import AnalogClock from "./components/analogClock";
+
+function App() {
+  return (
+    <div>
+      <AnalogClock />
+    </div>
+  );
+}
+
+export default App;
